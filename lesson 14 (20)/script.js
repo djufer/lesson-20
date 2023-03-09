16afa90f2fb559a2b56ce5a4bf948e66dc750aa4
Mykola Dzhufer <102813669+djufer@users.noreply.github.com>
2023-03-09 23:42:35 +0200

    Add files via upload

--- lesson 14 (20)/script.js
+++ lesson 14 (20)/script.js
@@ -6,110 +6,150 @@ let inpEmail = document.querySelector(".input-email");
 addUserButton.addEventListener("click", addUser);
 let getS = (sel) => document.querySelector(sel);
 let arr = [];
-function addUser() {
-    let obj = {
-        login: `${inpLogin.value}`,
-        password: `${inpPassword.value}`,
-        email: `${inpEmail.value}`,
-    };
-    arr.push(obj);
-    inpLogin.value = "";
-    inpPassword.value = "";
-    inpEmail.value = "";
 
-    render();
+// вілідація інпутів
+let loginRegExp = /^[a-zA-Z]{4,16}$/;
+let passwordRegExp = /^[\w_.-]{4,16}$/;
+let emailRegExp = /^[\w.-]+@[\w.-]+$/;
+
+inpLogin.addEventListener("input", () => {
+    if (loginRegExp.test(inpLogin.value)) {
+        inpLogin.classList.add("green-input");
+        inpLogin.classList.remove("red-input");
+    } else {
+        inpLogin.classList.add("red-input");
+        inpLogin.classList.remove("green-input");
+    }
+});
+inpPassword.addEventListener("input", () => {
+    if (passwordRegExp.test(inpPassword.value)) {
+        inpPassword.classList.add("green-input");
+        inpPassword.classList.remove("red-input");
+    } else {
+        inpPassword.classList.add("red-input");
+        inpPassword.classList.remove("green-input");
+    }
+});
+inpEmail.addEventListener("input", () => {
+    if (emailRegExp.test(inpEmail.value)) {
+        inpEmail.classList.add("green-input");
+        inpEmail.classList.remove("red-input");
+    } else {
+        inpEmail.classList.add("red-input");
+        inpEmail.classList.remove("green-input");
+    }
+});
+
+// функція додавання інфи з інпутів у масив та рендер
+
+function addUser(event) {
+    // тут також перевіряємо чи все правильно заповнено
+    if (
+        loginRegExp.test(inpLogin.value) &&
+        passwordRegExp.test(inpPassword.value) &&
+        emailRegExp.test(inpEmail.value)
+    ) {
+        let obj = {
+            login: `${inpLogin.value}`,
+            password: `${inpPassword.value}`,
+            email: `${inpEmail.value}`,
+        };
+        arr.push(obj);
+        inpLogin.value = "";
+        inpPassword.value = "";
+        inpEmail.value = "";
+
+        render();
+    } else {
+        alert("заповніть правильно поля");
+    }
 }
 
+//  ф-ція створення дом елементів таблиці і додавання туди інфи з масиву
 function render() {
-    // створюємо рядок
-    let tr = document.createElement("tr");
-    document.querySelector("table").appendChild(tr);
-    // створюємо  клітинки
-    let tdNum = document.createElement("td");
-    let tdLog = document.createElement("td");
-    let tdPassw = document.createElement("td");
-    let tdEmail = document.createElement("td");
-    let tdEdit = document.createElement("td");
-    let tdDelete = document.createElement("td");
-    // створення тегів кнопок
-    let editBtn = document.createElement("button");
-    let deleteBtn = document.createElement("button");
-    // додаємо кнопкам відповідні класи
-    editBtn.classList.add("edit-button");
-    deleteBtn.classList.add("delete-button");
-    // даємо назву кнопкам
-    let nameEditBtn = document.createTextNode("Edit");
-    let nameDeleteBtn = document.createTextNode("Delete");
+    document.querySelector("tbody").textContent = "";
+    for (let i = 0; i < arr.length; i++) {
+        // створюємо рядок
+        let tr = document.createElement("tr");
+        document.querySelector("tbody").appendChild(tr);
+        // створюємо  клітинки
+        let tdNum = document.createElement("td");
+        let tdLog = document.createElement("td");
+        let tdPassw = document.createElement("td");
+        let tdEmail = document.createElement("td");
+        let tdEdit = document.createElement("td");
+        let tdDelete = document.createElement("td");
+        // створення тегів кнопок
+        let editBtn = document.createElement("button");
+        let deleteBtn = document.createElement("button");
+        // додаємо відповідні класи
+        editBtn.classList.add("edit-button");
+        deleteBtn.classList.add("delete-button");
+        // даємо назву кнопкам
+        let nameEditBtn = document.createTextNode("Edit");
+        let nameDeleteBtn = document.createTextNode("Delete");
 
-    // вставляємо кнопку в клітинку
-    tdEdit.appendChild(editBtn);
-    tdDelete.appendChild(deleteBtn);
+        // вставляємо кнопку в клітинку
+        tdEdit.appendChild(editBtn);
+        tdDelete.appendChild(deleteBtn);
 
-    // створюємо вміст клітинок
-    let num = document.createTextNode(arr.length);
-    let log = document.createTextNode(`${arr[arr.length - 1].login}`);
-    let passw = document.createTextNode(`${arr[arr.length - 1].password}`);
-    let email = document.createTextNode(`${arr[arr.length - 1].email}`);
+        // створюємо вміст клітинок
+        let num = document.createTextNode(i + 1);
+        let log = document.createTextNode(`${arr[i].login}`);
+        let passw = document.createTextNode(`${arr[i].password}`);
+        let email = document.createTextNode(`${arr[i].email}`);
 
-    // вставляємо цей вміст у відповідні клітинки
-    tdNum.appendChild(num);
-    tdLog.appendChild(log);
-    tdPassw.appendChild(passw);
-    tdEmail.appendChild(email);
+        // вставляємо цей вміст у відповідні клітинки
+        tdNum.appendChild(num);
+        tdLog.appendChild(log);
+        tdPassw.appendChild(passw);
+        tdEmail.appendChild(email);
 
-    editBtn.appendChild(nameEditBtn);
-    deleteBtn.appendChild(nameDeleteBtn);
+        editBtn.appendChild(nameEditBtn);
+        deleteBtn.appendChild(nameDeleteBtn);
 
-    // вставляємо всі клітинки у рядок tr
+        // вставляємо всі клітинки у рядок tr
 
-    tr.appendChild(tdNum);
-    tr.appendChild(tdLog);
-    tr.appendChild(tdPassw);
-    tr.appendChild(tdEmail);
-    tr.appendChild(tdEdit);
-    tr.appendChild(tdDelete);
-
+        tr.appendChild(tdNum);
+        tr.appendChild(tdLog);
+        tr.appendChild(tdPassw);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdEdit);
+        tr.appendChild(tdDelete);
+    }
     //      видаленя рядка
-
-    let deleteButtons = document.querySelectorAll(".delete-button");
-    let deleteIndex;
-    for (let i = 0; i < deleteButtons.length; i++) {
-        deleteButtons[i].addEventListener("click", deleteUser);
-        deleteIndex = i;
+    let delButtons = document.querySelectorAll(".delete-button");
+    for (let i = 0; i < delButtons.length; i++) {
+        delButtons[i].addEventListener("click", () => {
+            arr.splice(i, 1);
+            render();
+        });
     }
-    function deleteUser(event) {
-        event.target.parentElement.parentElement.remove();
-        arr.splice(deleteIndex, 1);
-        console.log(arr);
-        let rows = document.querySelectorAll("tr");
-        for (let i = deleteIndex; i < rows.length; i++) {
-            rows[i + 1].firstElementChild.textContent--;
 
-            // -----тут матюкається firstElementChild, не знаю як зробити по іншому (
-        }
-    }
     // редагування рядка
 
     let editButtons = document.querySelectorAll(".edit-button");
-    let rows = document.querySelectorAll("tr");
+    // let rows = document.querySelectorAll("tr");
     for (let i = 0; i < editButtons.length; i++) {
         editButtons[i].addEventListener("click", () => {
-            inpLogin.value = rows[i + 1].children[1].textContent;
-            inpPassword.value = rows[i + 1].children[2].textContent;
-            inpEmail.value = rows[i + 1].children[3].textContent;
-
+            inpLogin.value = arr[i].login;
+            inpPassword.value = arr[i].password;
+            inpEmail.value = arr[i].email;
             getS(".add-user-button").style.zIndex = "-1";
+
             getS(".edit-user-button").addEventListener("click", () => {
-                rows[i + 1].children[1].textContent = inpLogin.value;
-                rows[i + 1].children[2].textContent = inpPassword.value;
-                rows[i + 1].children[3].textContent = inpEmail.value;
+                arr.splice(i, 1, {
+                    login: `${inpLogin.value}`,
+                    password: `${inpPassword.value}`,
+                    email: `${inpEmail.value}`,
+                });
 
                 inpLogin.value = "";
                 inpPassword.value = "";
                 inpEmail.value = "";
                 getS(".add-user-button").style.zIndex = "1";
+                render();
             });
         });
     }
-    console.log(arr);
 }
